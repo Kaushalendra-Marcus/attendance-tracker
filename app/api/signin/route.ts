e5bdd49a269952932155bf4291fce48496fe76eb
kaushalendra-marcus <yadavkausha4a5@gmail.com>
2025-08-06 13:53:47 +0530

    updating once again

--- app/api/signin/route.ts
+++ app/api/signin/route.ts
@@ -1,40 +1,33 @@
-import { connectToDB } from "@/lib/mongoose";
 import User from "@/lib/model/user.model";
+import { connectToDB } from "@/lib/mongoose";
 import { NextResponse } from "next/server";
-
 export async function POST(req: Request) {
     try {
         await connectToDB()
-        const { name, rollno, password, branch } = await req.json()
-        
-        if (!name || !password || !rollno || !branch) {
-            return NextResponse.json({ 
-                message: "All fields are required" 
-            }, { status: 400 })
+        const body = await req.json()
+        const { rollno, password } = body
+        if (!rollno || !password) {
+            return NextResponse.json({ message: "All fields are required" }, { status: 400 })
+        }
+        const user = await User.findOne({ rollno })
+        if (!user) {
+            return NextResponse.json({ message: "User does not exist" }, { status: 404 })
+        }
+        if (user.password !== password) {
+            return NextResponse.json({ message: "Invalid password" }, { status: 401 });
         }
 
-        const userExists = await User.findOne({ rollno })
-        if (userExists) {
-            return NextResponse.json({ 
-                message: "User already exists" 
-            }, { status: 400 })
-        }
-
-        const newUser = await User.create({ name, password, rollno, branch })
-        
         return NextResponse.json({
-            message: "User created successfully",
+            message: "Login successful",
             user: {
-                name: newUser.name,
-                rollno: newUser.rollno,
-                branch: newUser.branch,
+                name: user.name,
+                rollno: user.rollno,
+                branch: user.branch
             }
-        }, { status: 201 })
+        }, { status: 200 });
     } catch (err) {
         const error = err as Error
         console.log(error);
-        return NextResponse.json({ 
-            message: "Server Error" 
-        }, { status: 500 })
+        return NextResponse.json({ message: "Error in server" }, { status: 400 })
     }
 }
--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,18 +1,27 @@
 import { connectToDB } from "@/lib/mongoose";
 import User from "@/lib/model/user.model";
 import { NextResponse } from "next/server";
+
 export async function POST(req: Request) {
     try {
         await connectToDB()
-        const { name, rollno, password,  branch } = await req.json()
+        const { name, rollno, password, branch } = await req.json()
+        
         if (!name || !password || !rollno || !branch) {
-            return NextResponse.json({ errror: "All field required" }, { status: 400 })
+            return NextResponse.json({ 
+                message: "All fields are required" 
+            }, { status: 400 })
         }
+
         const userExists = await User.findOne({ rollno })
         if (userExists) {
-            return NextResponse.json({ error: "User alrady exist" }, { status: 400 })
+            return NextResponse.json({ 
+                message: "User already exists" 
+            }, { status: 400 })
         }
+
         const newUser = await User.create({ name, password, rollno, branch })
+        
         return NextResponse.json({
             message: "User created successfully",
             user: {
@@ -24,6 +33,8 @@ export async function POST(req: Request) {
     } catch (err) {
         const error = err as Error
         console.log(error);
-        return NextResponse.json({ error: "Server Error" }, { status: 500 })
+        return NextResponse.json({ 
+            message: "Server Error" 
+        }, { status: 500 })
     }
 }
